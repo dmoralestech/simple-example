94ba511e76ac8b69690d87b2e6866bd1482394fa
Philip A Senger <philip.a.senger@cngrgroup.com>
2017-06-13 18:58:47 +1000

    deployment

---
+++ build/js/version.js
@@ -0,0 +1,5 @@
+window.version = {
+  "name":        "simple-example",
+  "version":     "1.0.2",
+  "environment": "production"
+}
--- public/js/version.js
+++ public/js/version.js
@@ -1,5 +1,5 @@
 window.version = {
   "name":        "simple-example",
-  "version":     "1.0.2",
+  "version":     "1.0.3",
   "environment": "production"
 }
